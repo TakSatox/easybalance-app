711e916dd93081f462fc487df4c32c40c9b96c8b
Carlos Sato <carlostakeshi100@gmail.com>
2024-06-29 21:47:20 -0300

    added component cards

---
+++ src/components/DropdownButton.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="drop-down" @click="isEnabled = !isEnabled">
+        <span>{{ title }}</span>
+        <img v-if="isEnabled" id="user-arrow" :src="arrowUp"/>
+        <img v-else id="user-arrow" :src="arrowDown"/>
+    </div>
+
+</template>
+
+<script setup>
+import arrowDown from '@/assets/sidebar/arrow-down.png'
+import arrowUp from '@/assets/sidebar/arrow-up.png'
+import { ref } from 'vue';
+
+const props = defineProps({
+    title: String
+})
+
+
+const isEnabled = ref(false);
+
+</script>
